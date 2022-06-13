ed36521232289ce5342965fe4115300900073976
Hodpel <920998354@qq.com>
2022-06-13 23:52:03 +0800

    Update config.js

--- DB/config.js
+++ DB/config.js
@@ -1,3 +1,3 @@
-export const MONGODB_CONNECTION_STRING = "mongodb+srv://bihtyu:afvbLDbUTbN9rxm@shorturlonmongodb.fjepg.mongodb.net/myFirstDatabase?retryWrites=true&w=majority"
+export const MONGODB_CONNECTION_STRING = "mongodb+srv://hodpel:kuangwenxin123@cluster0.ts4spgp.mongodb.net/test?retryWrites=true&w=majority"
 
 export const DB_NAME = "SHORT_URL"
